5187f24aa37693594b18d16afac3f1a0aec9235b
bendorshai <bendorshai@gmail.com>
2016-08-16 22:31:11 +0300

    readme extended

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var itsadate = require('./model/compiler.js');
 
-var k = itsadate.getDateFromString("22/10/2014");
-console.log(k);
-
-k = itsadate.getDateFromString("1.9.1993");
+var k = itsadate.getDateFromString("1.9.1993");
 console.log(k);
 
 
