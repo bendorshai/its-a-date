cc7e309df6941f0880dea88b0fe5d12e878b20cc
bendorshai <bendorshai@gmail.com>
2016-08-17 00:13:21 +0300

    tail

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # its-a-date
 Parses a given English date string, absolute or relative, to a Date object.
 
-For example, given the string **"A day before the 4th of July 1985"** its-a-date returns the accurate Date object.
+For example, given the string **5 years ago at 15:44** its-a-date returns the accurate Date object.
+Other example might be **3 months after 11/01/1990** or simply **1/1/2009 14:00**
+
+Note: The pakage is still at very early stages and it is not complete.
--- model/tokens.js
+++ model/tokens.js
@@ -37,15 +37,27 @@ module.exports = exports = [
         ]
     },
     {
-        // Examples: 5 years ago
-        // or: 2 months ago
-        regex: /(?:\s|^)(\d+)\s(.+?)s?\sago(?:\s|$)/,
+        // Examples: 5 years ago. 2 months ago, 1 day before __
+        // or: 
+        regex: /(?:\s|^)(\d+)\s(.+?)s?\s(?:ago|before)(?:\s|$)/,
         affectsGenerator: function(match)
         {
             return [{
                 timeType: consts.timeTypes[match[2]],
                 affectType: consts.reltivity.relative,
                 value: match[1] * (-1)
+            }]
+        }
+    },
+    {
+        // Examples: 5 years since ___, 2 months after
+        regex: /(?:\s|^)(\d+)\s(.+?)s?\s(?:since|after)(?:\s|$)/,
+        affectsGenerator: function(match)
+        {
+            return [{
+                timeType: consts.timeTypes[match[2]],
+                affectType: consts.reltivity.relative,
+                value: match[1]
             }]
         }
     }
