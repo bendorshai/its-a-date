28992b1e34d2018deed0f982aa0530fb3b31f3d2
AviadG <aviadgal1@gmail.com>
2016-08-20 18:12:10 +0300

    Added parseToUnixTimestamp
    This function parse the dateString into unix timestamp format

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ exports.parse = function(dateString) {
     return compiler.getDateFromString(dateString);
 }
 
+// Parse and return in UnixTimestamp format
+exports.parseToUnixTimestamp = function(dateString) {
+     
+    return this.parse(dateString).getTime()/1000;
+}
+
 // Define the base format of the input string,
 // The default format is little-endian (day, month, year), e.g. 22/04/96.
 // This method will set it to middle-endian (month, day, year), e.g. 04/22/96.
