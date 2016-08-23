96029fba2d656d8cc0a266f5771d64b899a5cac3
Shahar Katz <skatz1990@gmail.com>
2016-08-23 17:06:31 +0300

    format code

--- model/compiler.js
+++ model/compiler.js
@@ -2,28 +2,27 @@ var parser = require('./parser.js');
 var interpreter = require('./interpreter.js')
 var State = require('./state.js');
 
-exports.getDateFromString = function(dateString, settings)
-{
+exports.getDateFromString = function (dateString, settings) {
     // New state each time
     var state = new State();
-    
+
     // Get all tokens found in date string
     var tokens = parser.getTokens(dateString);
-    
+
     if (tokens.length == 0) {
         throw 'ERROR: No token matched the string'
     }
-    
+
     // Execute all tokens
     for (var token of tokens) {
-        
+
         // Execute the token from the dateString onto the state
         interpreter.executeToken(token, dateString, state, settings);
     }
-    
+
     // Caculate the state
     state.calculateModifications();
-    
+
     // Get value
     return state.date;
 }
--- model/state.js
+++ model/state.js
@@ -6,11 +6,11 @@ const absolute = consts.reltivity.absolute;
 
 // ctor
 function State() {
-    
+
     // Init date as current
     this.date = new Date();
     this.date.setSeconds(0);
-    
+
     // Init the modification queue that logs the modification on each date part
     this.modificationQueues = [];
     this.modificationQueues[consts.timeTypes.year] = [];
@@ -31,71 +31,62 @@ module.exports = exports = State;
             value: 1993
         });
 */
-State.prototype.pushModification = function(timeType, modification)
-{
+State.prototype.pushModification = function (timeType, modification) {
     this.modificationQueues[timeType].push(modification);
 }
 
-State.prototype.calculateModifications = function()
-{
+State.prototype.calculateModifications = function () {
     // Foreach time type
-    for (var timeTypeIdx in consts.timeTypes)
-    {
+    for (var timeTypeIdx in consts.timeTypes) {
         var timeType = consts.timeTypes[timeTypeIdx];
-        
+
         // Execute it's modification queue, this affects the date object
         executeModificationsQueue(this.modificationQueues[timeType], timeType, this);
     }
 }
 
-function executeModificationsQueue(modifications, timeType, context)
-{
+function executeModificationsQueue(modifications, timeType, context) {
     if (modifications.length == 0) {
         return;
     }
-    
+
     // Count the absolute affect types (allowed between 1-0)
     var affectTypes = _.pluck(modifications, 'affectType');
-    var counts = _.countBy(affectTypes, function(type) {
+    var counts = _.countBy(affectTypes, function (type) {
         return type == absolute ? absolute : 'other';
     });
-    
+
     var absoluteCount = counts[absolute];
-    
+
     if (absoluteCount >= 2) {
         throw 'ERROR: Two or more absolute modifications are not allowed on the same date entity'
     }
-    
+
     // If there is one absolute I want to calculate it first, so I sort by it
-    if (absoluteCount == 1 && modifications.length > 1)
-    {
-        modifications = _.sortBy(modifications, function(mod) {
-           // Absolute will be first
-           return mod.affectType == absolute ? 1 : 2;
+    if (absoluteCount == 1 && modifications.length > 1) {
+        modifications = _.sortBy(modifications, function (mod) {
+            // Absolute will be first
+            return mod.affectType == absolute ? 1 : 2;
         });
     }
-    
+
     // Execute all modifications
-    for (var modification of modifications)
-    {
+    for (var modification of modifications) {
         executeModification(modification, timeType, context);
     }
 }
- 
-function executeModification(modification, timeType, context)
-{
+
+function executeModification(modification, timeType, context) {
     var value = modification.value;
-    
+
     // If avsolute
-    if (modification.affectType == absolute)
-    {
-        switch (timeType)
-        {
+    if (modification.affectType == absolute) {
+        switch (timeType) {
             case consts.timeTypes.year:
                 context.date.setFullYear(value);
                 return;
             case consts.timeTypes.month:
-                context.date.setMonth(value-1); // Months are wierd in js
+                context.date.setMonth(value - 1); // Months are wierd in js
                 return;
             case consts.timeTypes.day:
                 context.date.setDate(value);
@@ -106,18 +97,17 @@ function executeModification(modification, timeType, context)
             case consts.timeTypes.minute:
                 context.date.setMinutes(value);
                 return;
-            default: 
+            default:
                 throw 'ERROR: Unknown time type in modification excecution';
         }
     }
     // If relative
-    else if (modification.affectType == consts.reltivity.relative)
-    {    
+    else if (modification.affectType == consts.reltivity.relative) {
         var m = moment(context.date);
-        
+
         // Convert to moment time type
         var momentTimeType = timeType;
- 
-        context.date = m.add(value,momentTimeType).toDate();
+
+        context.date = m.add(value, momentTimeType).toDate();
     }
 }  
